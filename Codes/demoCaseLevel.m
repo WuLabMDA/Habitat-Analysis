2b742651aadf6682b914272a1bbe578a308b158f
MAminu <32936017+muhammadaminu47@users.noreply.github.com>
2023-06-15 12:50:45 -0500

    Add files via upload

---
+++ Codes/demoCaseLevel.m
@@ -0,0 +1,211 @@
+%	
+% Demo: Habitat Analysis (Case Level)
+% 
+% Reference:
+%
+%   1. Wu, Jia, Guohong Cao, Xiaoli Sun, Juheon Lee, Daniel L. Rubin, Sandy Napel, Allison W. Kurian, Bruce L. Daniel, and Ruijiang Li. 
+%   "Intratumoral spatial heterogeneity at perfusion MR imaging predicts recurrence-free survival
+%   in locally advanced breast cancer treated with neoadjuvant chemotherapy." 
+%   Radiology 288, no. 1 (2018): 26-35.
+%
+%   2. Aminu, Muhammad, Divya Yadav, Lingzhi Hong, Elliana Young, Paul Edelkamp Jr, Maliazurina Saad, Morteza Salehjahromi et al. 
+%   "Habitat Imaging Biomarkers for Diagnosis and Prognosis in Cancer Patients Infected with COVID-19." Cancers 15, no. 1 (2022): 275.
+%
+%   version 1.0 --May/2023
+%
+%   Written by: Muhammad Aminu (muhammadaminu47 AT gmail.com)
+%               Pingjun Chen (pingjunchen AT ieee.org)
+%               Jia Wu (jwu11 AT mdanderson.org)
+
+clear
+clc
+
+imgDir = 'V:\Amgad\PD-L1 Project\Aminu_Code\Data';
+datapath = dir(fullfile(imgDir,'CT'));
+segpath = dir(fullfile(imgDir,'tumor_mask'));
+datapath(1:2) = [];
+segpath(1:2) = [];
+
+rootDir = 'V:\Amgad\PD-L1 Project\Aminu_Code';
+Output=[rootDir '\Results\InputImages'];
+if ~exist(Output, 'dir')
+    mkdir(Output);
+end 
+
+SuperpixelsDir = fullfile(rootDir, 'Results', 'Superpixels');
+if ~exist(SuperpixelsDir, 'dir')
+    mkdir(SuperpixelsDir)
+end 
+
+% Windowing (contrast enhancement)
+lungs = [1500 -600]; % [WindowLevel WindowWidth]
+mediastinum = [350 50];
+lungMax = lungs(2) + lungs(1)/2; 
+lungMin = lungs(2) - lungs(1)/2; 
+medMax = mediastinum(2) + mediastinum(1)/2; 
+medMin = mediastinum(2) - mediastinum(1)/2; 
+
+% obtain patient list 
+numCases = numel(segpath);
+allSuperpixels = cell(numel(datapath), 1);
+idxCount = zeros(numel(datapath), 1);
+
+for i = 1:numel(datapath)
+    pipath = dir(fullfile(datapath(i).folder, datapath(i).name));
+%     pipath(1:2) = [];
+    disp(['Processing: ',datapath(i).name]);
+
+    % load CT scans
+    ctImg = niftiread(fullfile(pipath.folder, pipath.name));
+    ctInfo = niftiinfo(fullfile(pipath.folder, pipath.name));
+    imgSize = ctInfo.ImageSize;
+    pixelDim = ctInfo.PixelDimensions;
+    
+    if ctInfo.AdditiveOffset == -1024
+        ctImg = ctImg - 1024;
+    end
+
+    % load tumor mask
+    seg = logical(niftiread(fullfile(segpath(i).folder, segpath(i).name)));
+
+    % smooth the edges of the tumor mask
+    radius = 2;
+    decomposition = 0;
+    se = strel('disk',radius,decomposition);
+    maskSeg = imerode(seg, se);
+
+    % Normalize image
+    ctLung = mat2gray(ctImg, [lungMin lungMax]);
+    ctMed = mat2gray(ctImg, [medMin medMax]);
+
+    % compute bounding box
+    [boxBound] = computeBoundingBox(maskSeg);
+    maskSeg = maskSeg(boxBound(1,1):boxBound(1,2),...
+        boxBound(2,1):boxBound(2,2),boxBound(3,1):boxBound(3,2));
+
+    ctLung = ctLung(boxBound(1,1):boxBound(1,2),...
+        boxBound(2,1):boxBound(2,2),boxBound(3,1):boxBound(3,2));
+
+    ctMed = ctMed(boxBound(1,1):boxBound(1,2),...
+        boxBound(2,1):boxBound(2,2),boxBound(3,1):boxBound(3,2));
+
+    % compute local entropy
+    entLung = entropyfilt(double(ctLung));
+    entLung = rescale(entLung);
+    entMed = entropyfilt(double(ctMed));
+    entMed = rescale(entMed);
+
+    % get region of interest
+    ctLung_roi = ctLung(maskSeg);
+    ctMed_roi = ctMed(maskSeg);
+    entCT_roi = entLung(maskSeg);
+    entMed_roi = entMed(maskSeg);
+
+    % Look into tumor region only
+    ctLung = ctLung.*maskSeg;
+    ctMed = ctMed.*maskSeg;
+    entLung = entLung.*maskSeg;
+    entMed = entMed.*maskSeg;
+
+    % Fuse different modalities
+    imgFused = imadd(imadd(ctLung, entLung), imadd(ctMed, entMed));  % fuse multiple 3D images, imlincomb, imadd, imcompliment, ...
+    tempFused = imgFused(maskSeg);
+
+    % SLIC superpixel ooversegmentation
+    if sum(maskSeg,'all') < 1000
+        numSuperpixels = 30;
+    elseif sum(maskSeg,'all') > 1000 && sum(maskSeg,'all') < 10000
+        numSuperpixels = 50;
+    elseif sum(maskSeg,'all') > 10000 && sum(maskSeg,'all') < 100000
+        numSuperpixels = 60;
+    else
+        numSuperpixels = 80;
+    end
+
+    [L, N] = superpixels3(imgFused, numSuperpixels, 'Method', 'slic', 'Compactness', 1e-2);
+
+    indices = find(maskSeg==1);
+    [rows, cols, slices] = ind2sub(size(maskSeg), indices); % coordinates
+
+    features = [double(ctLung_roi), double(entCT_roi), double(ctMed_roi), double(entMed_roi),...
+        double(tempFused), double(rows), double(cols), double(slices)];
+
+    idxCL = L(maskSeg);
+
+    superpixel = zeros(length(unique(idxCL)), 40);
+    label = unique(idxCL);
+    for kk = 1:length(label)
+        ind = find(idxCL==label(kk));
+        clusterData = features(ind,1:end-4);
+        Sfea = histFeas(clusterData);
+        superpixel(kk,:) = Sfea;
+    end
+    allSuperpixels{i} = superpixel;
+    idxCount(i) = length(unique(idxCL));
+
+    % save outputs
+    name = string(extractBetween(segpath(i).name,"RTS_","_P.nii.gz"));
+    filename_input = [Output '\' convertStringsToChars(name) '.png'];
+    f = figure('visible', 'on');
+    %     f.WindowState = 'maximized';
+
+    TumorVox = sum(maskSeg,1);
+    TumorVox = sum(TumorVox,2);
+    TumorVox = TumorVox(:);
+    [~,idx] = max(TumorVox);
+    BW = maskSeg(:,:,idx);
+    [B1,L1] = bwboundaries(BW);
+
+    subplot(2,2,1)
+    imshow(ctLung(:,:,idx),[])
+    title('CT Lung')
+    hold on
+    for k=1:length(B1)
+        boundary = B1{k};
+        plot(boundary(:,2), boundary(:,1),...
+            'r-', 'LineWidth', 1)
+    end
+    hold off
+
+    subplot(2,2,2)
+    imshow(ctMed(:,:,idx),[])
+    title('CT Med')
+    hold on
+    for k=1:length(B1)
+        boundary = B1{k};
+        plot(boundary(:,2), boundary(:,1),...
+            'r-', 'LineWidth', 1)
+    end
+    hold off
+
+    subplot(2,2,3);
+    imshow(entLung(:,:,idx),[])
+    title('entropy CT Lung')
+    hold on
+    for k=1:length(B1)
+        boundary = B1{k};
+        plot(boundary(:,2), boundary(:,1),...
+            'r-', 'LineWidth', 1)
+    end
+    hold off
+
+    subplot(2,2,4);
+    imshow(entMed(:,:,idx),[])
+    title('entropy CT Med')
+    hold on
+    for k=1:length(B1)
+        boundary = B1{k};
+        plot(boundary(:,2), boundary(:,1),...
+            'r-', 'LineWidth', 1)
+    end
+    hold off
+
+    saveas(f,filename_input)
+    close(f)
+
+    p_intratumoral_path = fullfile(SuperpixelsDir, name);
+    save(p_intratumoral_path, 'features', 'idxCL', 'indices', 'label', 'imgSize', 'pixelDim');
+end
+path = fullfile(rootDir, 'Results', 'supPixels');
+allSuperpixels = cell2mat(allSuperpixels);
+save(path, 'allSuperpixels', 'idxCount');
---
+++ Codes/demoMSI.m
@@ -0,0 +1,146 @@
+%	
+% Demo: Multiregional spatial interaction (MSI) feature extraction
+% 
+% Reference:
+%
+%   1. Wu, Jia, Guohong Cao, Xiaoli Sun, Juheon Lee, Daniel L. Rubin, Sandy Napel, Allison W. Kurian, Bruce L. Daniel, and Ruijiang Li. 
+%   "Intratumoral spatial heterogeneity at perfusion MR imaging predicts recurrence-free survival
+%   in locally advanced breast cancer treated with neoadjuvant chemotherapy." 
+%   Radiology 288, no. 1 (2018): 26-35.
+%
+%   2. Aminu, Muhammad, Divya Yadav, Lingzhi Hong, Elliana Young, Paul Edelkamp Jr, Maliazurina Saad, Morteza Salehjahromi et al. 
+%   "Habitat Imaging Biomarkers for Diagnosis and Prognosis in Cancer Patients Infected with COVID-19." Cancers 15, no. 1 (2022): 275.
+%
+%   version 1.0 --May/2023
+%
+%   Written by: Muhammad Aminu (muhammadaminu47 AT gmail.com)
+%               Pingjun Chen (pingjunchen AT ieee.org)
+%               Jia Wu (jwu11 AT mdanderson.org)
+
+clc
+clear
+
+rootDir = '...\All Data';
+
+ClusterNum = 5;
+tumorClusterDir = fullfile(rootDir, 'Results', 'TumorPartitions');
+SuperpixelsDir = fullfile(rootDir, 'Results', 'Superpixels');
+
+patientList = dir(fullfile(tumorClusterDir, '*.mat'));
+numPatient = length(patientList);
+msiPath = fullfile(rootDir, 'Results', 'MSI features');
+if ~exist(msiPath, 'dir')
+    mkdir(msiPath)
+end
+
+MSIFeatures = cell(numPatient, 2);
+for p = 1:numPatient
+    caseName = patientList(p).name;
+    [~, pName, ~] = fileparts(caseName);
+    disp(['Processing ', num2str(p), '/', num2str(numPatient), ' ', pName]);
+    feaPath = fullfile(SuperpixelsDir, caseName);
+    tumorPartPath = fullfile(tumorClusterDir, caseName);
+    load(feaPath, 'features', 'idxCL', 'imgSize', 'pixelDim');
+    load(tumorPartPath);
+    
+    % reconstruct the tumor cube
+    row = features(:,6) - min(features(:,6));
+    col = features(:,7) - min(features(:,7));
+    slice = features(:,8) - min(features(:,8)); 
+    map = zeros(max(row)+3, max(col)+3, max(slice)+3);
+    mask = zeros(max(row)+3, max(col)+3, max(slice)+3);
+    
+    % populate map and mask
+    for i = 1:length(idxCL)
+        map(row(i)+2,col(i)+2,slice(i)+2) = idxPL(i);
+        mask(row(i)+2,col(i)+2,slice(i)+2) = 1;
+    end
+    
+    % build MSI matrix
+    ind = find(mask);
+    [idx1, idx2, idx3] = ind2sub(size(mask), ind);
+    matrixHabitat = zeros(ClusterNum + 1); 
+    for pt = 1:length(ind)
+        % use 8 connectivity model in 2D
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt)-1,idx2(pt)-1,idx3(pt));
+        
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt)-1,idx2(pt),idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt)-1,idx2(pt)+1,idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt),idx2(pt)-1,idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt),idx2(pt)+1,idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt)+1,idx2(pt)-1,idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt)+1,idx2(pt),idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+
+        % the habitat label from pre and mid habitat
+        IDX_1 = map(idx1(pt),idx2(pt),idx3(pt));
+        IDX_2 = map(idx1(pt)+1,idx2(pt)+1,idx3(pt));
+        % summarize into the matrix
+        matrixHabitat(IDX_1+1,IDX_2+1) = matrixHabitat(IDX_1+1,IDX_2+1)+1;
+    end   
+    
+    matrixHabitat(1,1) = 0;    
+    for k = 1:size(matrixHabitat,1)
+        matrixHabitat(1,k) = matrixHabitat(k,1);
+    end
+    
+    stats = graycoprops(matrixHabitat,{'contrast','homogeneity','correlation','energy'});
+    glcmFeatures = [stats.Contrast,stats.Homogeneity,stats.Correlation,stats.Energy];
+    
+    matrixHabitat = matrixHabitat.*prod(pixelDim);
+    x = diag(matrixHabitat);
+    x = x(2:end);
+    m = tril(true(size(matrixHabitat)),-1);
+    y = matrixHabitat(m);
+    
+    sum_mth = sum(matrixHabitat(:));
+    norm_matrix_habitat = matrixHabitat./sum_mth;
+    xx = diag(norm_matrix_habitat);
+    xx = xx(2:end);
+    n = tril(true(size(norm_matrix_habitat)),-1);
+    yy = norm_matrix_habitat(n);
+    
+    feaMSI = [glcmFeatures x' y' xx' yy'];  % first 4 are GLCM, k habitat diagonal components, off diagnonal , normalized, 
+    MSIFeatures{p,1} = pName;
+    MSIFeatures{p,2} = feaMSI;
+    
+end
+save(msiPath, 'MSIFeatures');
+
+IDS = MSIFeatures(:,1);
+tbl = array2table(cell2mat(MSIFeatures(:,2)));
+tbl.Properties.RowNames = IDS;
+
+writetable(tbl,'MSIFeaturesGEMINI.xlsx','WriteRowNames',true)
